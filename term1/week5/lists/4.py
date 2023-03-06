eb97e2aba37d24ccbf79aa41a7b8774c6e78f356
FusionSid <siddheshadsv@icloud.com>
2023-03-07 10:08:33 +1300

    feat: complete list 4 exercise to more sketchier solution

--- term1/week5/lists/4.py
+++ term1/week5/lists/4.py
@@ -1,3 +1,5 @@
-list1 = ["M", "na", "i", "Ke"]
+list1 = ["M", "na", "i", "Ke", "too", "long", "rrrrrr"]
 list2 = ["y", "me", "s", "lly"]
-print(" ".join([i + j for i, j in list(zip(list1, list2))]))
+shorter = min([len(list1), len(list2)])
+list3 = [i + j for i, j in list(zip(list1, list2))] + list1[shorter:]+list2[shorter:]
+print(" ".join(list3))
